27bd50169e43595b7b82e7a696ec287448c35244
sven.zengaffi <sven.zengaffi@hes-so.ch>
2022-10-07 09:21:08 +0200

    changes initFirebase.js and Register.js

--- src/initFirebase.js
+++ src/initFirebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 // Configure Firebase.
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   // Other configuration options, such as the Realtime Database / Firestore details...
 };
-
 const app = initializeApp(config);
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
 export default app;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,25 +2,26 @@ import UserForm from "../components/UserForm";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../initFirebase";
 import { useNavigate } from "react-router-dom";
-
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../initFirebase";
 export default function Register() {
   const navigate = useNavigate();
-
+  let userUID;
   const handleRegister = async (e, email, password) => {
     e.preventDefault();
-
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      await createUserWithEmailAndPassword(auth, email, password).then(data => {
+        console.log("Je suis laaaa");
+        userUID = data.user.uid;
+        db.collection("user").doc(userUID).set({
+          Role: "PNJ"
+        });
+      });
       navigate("/");
     } catch (e) {
       console.error(e);
     }
   };
-
   return (
-    <div>
-      <h1>Register</h1>
-      <UserForm handleSubmit={handleRegister} submitButtonLabel="Register" />
-    </div>
-  );
+      <div>      <h1>Register</h1>      <UserForm handleSubmit={handleRegister} submitButtonLabel="Register" />    </div>  );
 }

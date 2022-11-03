dcb81382d0de78feaafbc1dcf4846a2931fc6b9c
sven.zengaffi <sven.zengaffi@hes-so.ch>
2022-11-03 10:46:43 +0100

    changes AdminPage Sven

--- src/pages/AdminForm.js
+++ src/pages/AdminForm.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {collection, doc, getDocs, updateDoc} from "firebase/firestore";
 import {db} from "../initFirebase";
-
+//
 function AdminForm() {
 
     let [docs, setDocs] = useState({});
